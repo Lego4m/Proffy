6b17173e0e44faeebe4b0720c55edab87d75a3f3
Leonardo Maron <leo22.maron@hotmail.com>
2020-08-03 15:07:37 -0300

    WEB - Criação do App web * Utilização da medida REM * Configuração de rotas

---
+++ web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
+import './styles.css';
+
+function TeacherItem() {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img src="https://avatars3.githubusercontent.com/u/39628486?s=460&u=796c8fcafa8bb0d61f8cbf9f025011adf199e83d&v=4" alt="Leonardo Maron"/>
+        <div>
+          <strong>Leonardo Maron</strong>
+          <span>Química</span>
+        </div>
+      </header>
+
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+        <br/> <br/>
+        Apaixonado por explodir coisas em laboratório e por mudar a vida de pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+      </p>
+
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ 80,00</strong>
+        </p>
+
+        <button type="button">
+          <img src={whatsappIcon} alt="Whatsapp"/>
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  )
+}
+
+export default TeacherItem;
